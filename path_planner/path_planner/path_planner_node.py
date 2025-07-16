00da7ad1931f38b192ee853582fbbcb310e4f087
yyy-wsl <1830979240@qq.com>
2025-07-16 21:21:16 +0800

    ROS: 路径规划服务

--- path_planner/path_planner/path_planner_node.py
+++ path_planner/path_planner/path_planner_node.py
@@ -2,26 +2,38 @@ import numpy as np
 import rclpy
 from geometry_msgs.msg import Pose2D
 from nav_msgs.msg import OccupancyGrid
-from path_msg.msg import HPath
-from path_planner.hybrid_a_star_planner import HMap, HybridAStarPlanner
+from path_msgs.msg import HPath as HPathROS
+from path_msgs.srv import PathPlanning
+from path_planner.hybrid_a_star_planner import HMap, HPath, HybridAStarPlanner
 from rclpy.node import Node
 from utils import Pose2D as MyPose2D
 
 
 class HybridAStarNode(Node):
     def __init__(self):
-        super().__init__("_node")
+        super().__init__("path_planner_node")
+        self.INFO = self.get_logger().info
+        self.WARN = self.get_logger().warn
         self.map_sub = self.create_subscription(OccupancyGrid, "/map", self.map_callback, 10)
-        self.path_pub = self.create_publisher(HPath, "/plan_path", 10)
+        # self.path_pub = self.create_publisher(HPathROS, "/plan_path", 10)
 
+        # 规划服务
+        self.planning_srv = self.create_service(PathPlanning, "plan_path", self.handle_plan_path)
+
+        self.map_msg = None
         self.map = None
         self.planner = None
 
-        self.get_logger().info("Hybrid A* Node initialized, waiting for map...")
+        self.INFO("路径规划器初始化完毕")
 
-    def map_callback(self, msg: OccupancyGrid):
-        self.get_logger().info("Received map")
-        # 将ROS地图转为numpy bool数组（True为障碍物）
+    def handle_plan_path(self, request: PathPlanning.Request, response: PathPlanning.Response):
+        if self.map_msg is None:
+            response.success = False
+            response.reason = "未收到地图"
+            return response
+
+        # 缓存的ROS地图信息，需要时再构造规划器实例
+        msg = self.map_msg
         width = msg.info.width
         height = msg.info.height
         resolution = msg.info.resolution
@@ -29,16 +41,29 @@ class HybridAStarNode(Node):
         ob_map = data > 50  # 通常占用率大于50算作障碍物
 
         self.map = HMap(ob_map, resolution=resolution)
-        self.planner = HybridAStarPlanner(self.map)
+        self.planner = HybridAStarPlanner(self.map)  # TODO: map_msg的origin
 
-        # 示例起终点
-        start = MyPose2D(1.0, 1.0, 0.0)
-        goal = MyPose2D(8.0, 8.0, 0.0)
-        path = self.planner.planning(start, goal)
-        self.publish_path(path)
+        start = MyPose2D(request.start.x, request.start.y, request.start.theta)
+        goal = MyPose2D(request.goal.x, request.goal.y, request.goal.theta)
 
-    def publish_path(self, path):
-        msg = HPath()
+        path, plan_result = self.planner.planning(start, goal)
+        if path is None:
+            response.success = False
+            response.reason = plan_result
+            self.WARN(plan_result)
+        else:
+            response.success = True
+            response.reason = f"规划路径成功"
+            response.path = self.convert_to_hpath_msg(path)  # 路径放响应中
+
+        return response
+
+    def map_callback(self, msg: OccupancyGrid):
+        self.map_msg = msg
+
+    @staticmethod
+    def convert_to_hpath_msg(path: HPath) -> HPathROS:
+        msg = HPathROS()
         msg.directions = path.direction_list
 
         for x, y, yaw in zip(path.x_list, path.y_list, path.yaw_list):
@@ -48,8 +73,7 @@ class HybridAStarNode(Node):
             pose.theta = yaw
             msg.poses.append(pose)
 
-        self.path_pub.publish(msg)
-        self.get_logger().info(f"Published path with {len(msg.poses)} poses")
+        return msg
 
 
 def main(args=None):
